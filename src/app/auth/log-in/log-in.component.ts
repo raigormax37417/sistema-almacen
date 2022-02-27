e7f4f16aaddf247949e41466cc41675be5bac1f9
Rebjai <rebjai@github.com>
2022-02-27 13:51:46 -0600

    first draft for logIn form

--- src/app/auth/log-in/log-in.component.ts
+++ src/app/auth/log-in/log-in.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup
+  constructor(private fb: FormBuilder, private router : Router, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: '',
+      password: ''
+    })
+   }
 
   ngOnInit(): void {
+   
   }
+  login(){
+    if(this.loginForm.invalid)
+    return alert('credenciales pls')
+    
+    let user = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+    }
+    this.authService.loginEmail(user)
+  }
+  forgotPassword(){}
 
 }
