e19ec231891264770f3787f6366c3fa894ca7a59
Rebjai <rebjai@github.com>
2022-06-22 21:22:56 -0500

    bugfix set qrcode

--- src/app/students/qrcode/qrcode.component.ts
+++ src/app/students/qrcode/qrcode.component.ts
@@ -21,37 +21,49 @@ export class QrcodeComponent implements OnInit {
   private id: any[] = [];
   private unusubscribe: any;
   constructor(private _tools: ToolsService, private _profile: ProfileService) {
-
+    this._profile.profile.subscribe(profile => {
+      this.uid = profile?.id;
+      this.getQrCode();
+      this.generateQR(this.uid!)
+      console.log({ uid: this.uid });
+    })
   }
 
   ngOnInit(): void {
-    this._profile.profile.subscribe(profile => {
-      this.uid = profile?.id;
 
-      this.generateQR(this.uid!);
-      console.log({ uid: this.uid });
-      this.getQrCode();
-    })
   }
   getQrCode() {
+    console.log({ uid: this.uid });
     const fire = this._tools.getDataFirestore<Pedido>(this.path);
     const userActiveOrder = query(fire, where("status", "==", "solicitado"), where("profile.id", "==", this.uid));
     this.unusubscribe = onSnapshot(userActiveOrder, (QuerySnapshot) => {
       const dataFirestore: DocumentData[] = [];
       QuerySnapshot.forEach(doc => {
         dataFirestore.push(doc.data());
-        this.data = dataFirestore;
         this.id.push(doc.id);
-        console.log(this.data);
+        console.log({ data: this.data });
       });
+      this.data = dataFirestore as unknown as any[];
+      if (this.data.length) {
+        console.log({ datalengt: this.data[0].id });
 
+        // this.dataQR= this.data[0].id
+        // this.widthValue = 256;
+        // console.log({width: this.widthValue});
+
+      }
+      console.log({ data: this.data[0].status });
+      console.log({ dataFirestore });
+      console.log({ dataqr: this.dataQR });
+
+      this.generateQR(this.data[0].id);
     });
   }
   generateQR(uid: string) {
-    this.data.forEach( (res, index) => {
+    console.log('yuid', uid);
+    // console.log(data);
+    this.dataQR = uid
+    this.widthValue = 256;
 
-      uid === res.id ? this.dataQR = this.id[index] : this.dataQR = "";
-      this.widthValue = 256;
-    })
   }
 }
